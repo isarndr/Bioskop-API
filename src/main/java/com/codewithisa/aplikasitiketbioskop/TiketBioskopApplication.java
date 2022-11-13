9ef24ce6e1379ba051871a8e6761ec310eacfae8
Isa Randra <isarandra@gmail.com>
2022-11-14 02:17:24 +0700

    preparing for controller setup

--- src/main/java/com/codewithisa/aplikasitiketbioskop/TiketBioskopApplication.java
+++ src/main/java/com/codewithisa/aplikasitiketbioskop/TiketBioskopApplication.java
@@ -6,25 +6,23 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Scanner;
-
 @SpringBootApplication
-public class AplikasiTiketBioskopApplication implements CommandLineRunner {
+public class TiketBioskopApplication implements CommandLineRunner {
 
 	@Autowired
 	MenuService menuService;
 
 	public static void main(String[] args) {
-		SpringApplication.run(AplikasiTiketBioskopApplication.class, args);
+		SpringApplication.run(TiketBioskopApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println();
-		System.out.println("Aplikasi pemesanan tiket bioskop");
-		System.out.println();
-		Scanner scanner = new Scanner(System.in);
-		menuService.menu(scanner);
-		scanner.close();
+//		System.out.println();
+//		System.out.println("Aplikasi pemesanan tiket bioskop");
+//		System.out.println();
+//		Scanner scanner = new Scanner(System.in);
+//		menuService.menu(scanner);
+//		scanner.close();
 	}
 }
